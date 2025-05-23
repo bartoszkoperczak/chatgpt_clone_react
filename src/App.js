daade16299e6618b322aa2237b79f24cde0c8b13
bartoszkoperczak <koperczakbartosz@gmail.com>
2025-05-23 23:57:22 +0200

    Created basic html + css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,86 @@
-import logo from './logo.svg';
 import './App.css';
+import gptLogo from './assets/chatgpt.svg';
+import addBtn from './assets/add-30.png';
+import queryIcon from './assets/message.svg';
+import home from './assets/home.svg';
+import saved from './assets/bookmark.svg';
+import rocket from './assets/rocket.svg';
+import sendBtn from './assets/send.svg';
+import userIcon from './assets/user-icon.png';
+import gptImgLogo from './assets/chatgptLogo.svg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="sideBar">
+        <div className="upperSide">
+          <div className="upperSideTop">
+            <img src={gptLogo} alt="Logo" className="logo" />
+            <span className="brand">ChatGPT Clone</span>
+          </div>
+          <button className="midBtn">
+            <img src={addBtn} alt="new chat" className="addBtn" />
+            New Chat
+          </button>
+          <div className="upperSideBottom">
+            <button className="query">
+              <img src={queryIcon} alt="query" />
+              What is Programming?
+            </button>
+            <button className="query">
+              <img src={queryIcon} alt="query" />
+              How to use an API?
+            </button>
+          </div>
+        </div>
+
+        <div className="lowerSide">
+          <div className="listItems">
+            <img src={home} alt="Home" className="listitemsImg" />
+            Home
+          </div>
+          <div className="listItems">
+            <img src={saved} alt="Saved" className="listitemsImg" />
+            Saved
+          </div>
+          <div className="listItems">
+            <img src={rocket} alt="Get a Pro" className="listitemsImg" />
+            Get a Pro
+          </div>
+        </div>
+      </div>
+      <div className="main">
+        <div className="chats">
+          <div className="chat">
+            <img className="chatImg" src={userIcon} alt="" />
+            <p className="txt">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. At harum
+              vero architecto, eius illo perspiciatis saepe soluta aspernatur
+              cupiditate? Provident!
+            </p>
+          </div>
+          <div className="chat bot">
+            <img className="chatImg" src={gptImgLogo} alt="" />
+            <p className="txt">
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. At harum
+              vero architecto, eius illo perspiciatis saepe soluta aspernatur
+              cupiditate? Provident!
+            </p>
+          </div>
+        </div>
+        <div className="chatFooter">
+          <div className="inp">
+            <input type="text" placeholder="Send a message..." />{' '}
+            <button className="send">
+              <img src={sendBtn} alt="Send" />
+            </button>
+          </div>
+          <p>
+            ChatGPT may produce inaccurate information about people, places or
+            facts. ChatGPT May 23 Version.
+          </p>
+        </div>
+      </div>
     </div>
   );
 }
